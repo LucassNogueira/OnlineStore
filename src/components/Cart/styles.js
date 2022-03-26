69d9f7cc9ac12f21f3ba7f00f71abe4b920e313b
LucassNogueira <simplylucasjason@gmail.com>
2022-03-25 20:12:04 -0500

    had styles inccorect

--- src/components/Cart/styles.js
+++ src/components/Cart/styles.js
@@ -1,52 +1,29 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-  appBar: {
-    position: "relative",
-  },
   toolbar: theme.mixins.toolbar,
-  layout: {
+  title: {
     marginTop: "5%",
-    width: "auto",
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
-      width: 600,
-      marginLeft: "auto",
-      marginRight: "auto",
-    },
   },
-  paper: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    padding: theme.spacing(2),
+  emptyButton: {
+    minWidth: "150px",
     [theme.breakpoints.down("xs")]: {
-      width: "100%",
-      marginTop: 60,
+      marginBottom: "5px",
     },
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      padding: theme.spacing(3),
+    [theme.breakpoints.up("xs")]: {
+      marginRight: "20px",
     },
   },
-  stepper: {
-    padding: theme.spacing(3, 0, 5),
+  checkoutButton: {
+    minWidth: "150px",
   },
-  buttons: {
+  link: {
+    textDecoration: "none",
+  },
+  cardDetails: {
     display: "flex",
-    justifyContent: "flex-end",
-  },
-  button: {
-    marginTop: theme.spacing(3),
-    marginLeft: theme.spacing(1),
-  },
-  divider: {
-    margin: "20px 0",
-  },
-  spinner: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    marginTop: "10%",
+    width: "100%",
+    justifyContent: "space-between",
   },
 }));
